a8f8e5f475f2695712fbbe220709017b8653a4ed
Aiden <162728708+AidenNabavi@users.noreply.github.com>
2025-11-01 14:24:45 +0330

    Add files via upload

---
+++ lib/interfaces/IDetailedERC20.sol
@@ -0,0 +1,11 @@
+// نسخه پیشرفته erc20
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.28;
+
+import "../openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IDetailedERC20 is IERC20 {
+  function name() external returns (string memory);
+  function symbol() external returns (string memory);
+  function decimals() external returns (uint8);
+}
